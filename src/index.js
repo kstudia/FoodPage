89ed5069135977ac98f79320822ad0d8bcd86d59
kstudia <k.studia@proton.me>
2023-07-06 14:05:33 +0200

    dasasfasf

--- src/index.js
+++ src/index.js
@@ -17,42 +17,6 @@ app.use(express.static(statick_path));
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.post("/register", async (req, res) => {
-  try {
-    const password = req.body.passwordd;
-    const confpassword = req.body.confpassword;
-
-    if (password === confpassword) {
-      const registerClient = new Register({
-        email: req.body.emailvalue,
-        password: req.body.passwordd,
-        confpassword: req.body.confpassword,
-      });
-      const registered = await registerClient.save();
-      use = res.redirect("/")
-    } else {
-      res.send("password are not matching");
-    }
-  }catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.post("/login", async (req, res) => {
-  try {
-    const email = req.body.emailvaluelogin;
-    const password = req.body.passworddlogin;
-    console.log(`email ${email}, are correct and password ${password}`);
-    const userEmail = await Register.findOne({ email });
-    if (userEmail.password === password) {
-        use = res.redirect("/")
-    } else {
-      res.send("Password are not matching conditions");
-    }
-  } catch (error) {
-    res.status(400).send("Invalid email");
-  }
-});
 
 
 const connectDB = async ()=>{
